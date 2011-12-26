587ab9859c628462d6f74ba4da9d64aed1595dc3
Tate Johnson <tate@tatey.com>
2011-12-26 14:18:06 +1000

    Definition lets you append columns

--- lib/conformist/definition.rb
+++ lib/conformist/definition.rb
@@ -1,9 +1,17 @@
 module Conformist  
   class Definition
+    extend Forwardable
+    
     attr_accessor :columns
+    
+    delegate [:[], :size] => :columns
     
     def initialize
       self.columns = []
+    end
+
+    def column *args
+      columns << Column.new(*args)
     end
   end
 end
--- test/conformist/definition_test.rb
+++ test/conformist/definition_test.rb
@@ -5,4 +5,24 @@ class DefinitionTest < MiniTest::Unit::TestCase
     definition = Definition.new
     assert_empty definition.columns
   end
+  
+  def test_column
+    definition = Definition.new
+    definition.column :a, 0
+    definition.column :b, 1
+    assert_equal 2, definition.size
+  end
+  
+  def test_reference
+    definition = Definition.new
+    definition.columns = ['a', 'b']
+    assert_equal 'a', definition[0]
+    assert_equal 'b', definition[1]
+  end
+  
+  def test_size
+    definition = Definition.new
+    definition.columns = [1, 2]
+    assert_equal 2, definition.size
+  end
 end
