06b81bfa5bdfddba77471d01be7d5e976623aa87
Tate Johnson <tate@tatey.com>
2011-12-28 15:30:46 +1000

    Anything that quacks like strip

--- lib/conformist/column.rb
+++ lib/conformist/column.rb
@@ -1,26 +1,23 @@
 module Conformist
   class Column
-    attr_reader :name, :indexes, :preprocessor
+    attr_accessor :name, :indexes, :preprocessor
         
     def initialize name, *indexes, &preprocessor
-      @name, @indexes, @preprocessor = name, indexes, preprocessor
+      self.name         = name
+      self.indexes      = indexes
+      self.preprocessor = preprocessor
     end
     
-    # Map column(s) into a single value, strips whitespace and performs
-    # any preprocessing. Takes a +row+ argument that should behave as an
-    # array-like object.
-    #
-    # Example:
-    #
-    #   row    = ['Hello', 'World']
-    #   column = Column.new :first, 0
-    #   column.value_in row # => 'Hello'
-    #
-    # Returns preprocessed value.
-    def values_in row      
-      values = indexes.map { |index| row[index] ? row[index].strip : nil }
-      values = values.first if values.size == 1
-      if preprocessor.respond_to? :call
+    def values_in array
+      values = array.values_at(*indexes).map do |value|
+        if value.respond_to? :strip
+          value.strip
+        else
+          value
+        end
+      end    
+      values = values.first if values.size == 1  
+      if preprocessor
         preprocessor.call values
       else
         values
