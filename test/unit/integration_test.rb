4dbc235180bce11fd98cb18802938351fd88d0d6
Tate Johnson <tate@tatey.com>
2011-12-29 15:20:22 +1000

    csv -> open_csv

--- test/unit/integration_test.rb
+++ test/unit/integration_test.rb
@@ -4,32 +4,29 @@ require 'definitions/fcc'
 require 'helper'
 
 class IntegrationTest < MiniTest::Unit::TestCase
-  def fixture filename
-    File.expand_path "../../fixtures/#{filename}", __FILE__
-  end
-  
-  def csv filename, options = {}
+  def open_csv filename, options = {}
+    path = File.expand_path "../../fixtures/#{filename}", __FILE__
     if CSV.method(:open).arity == -3
-      CSV.open fixture(filename), 'r', options[:col_sep]
+      CSV.open path, 'r', options[:col_sep]
     else
-      CSV.open fixture(filename), options
+      CSV.open path, options
     end
   end
 
   def test_class_with_csv
-    enumerable = ACMA.conform csv('acma.csv')
+    enumerable = ACMA.conform open_csv('acma.csv')
     last       = enumerable.to_a.last
     assert_equal HashWithReaders.new(:name=>'CRAFERS', :callsign=>'ADS10', :latitude=>'34 58 57S'), last
   end
   
   def test_inherited_class_with_csv
-    enumerable = ACMA::Digital.conform csv('acma.csv')
+    enumerable = ACMA::Digital.conform open_csv('acma.csv')
     last       = enumerable.to_a.last
     assert_equal HashWithReaders.new(:name=>'CRAFERS', :callsign=>'ADS10', :latitude=>'34 58 57S', :signal_type => 'digital'), last
   end
   
   def test_class_with_psv
-    enumerable = FCC.conform csv('fcc.txt', :col_sep => '|')
+    enumerable = FCC.conform open_csv('fcc.txt', :col_sep => '|')
     last       = enumerable.to_a.last
     assert_equal HashWithReaders.new(:name => 'LOS ANGELES, CA', :callsign => 'KVTU-LP', :latitude => '34 13 38.00 N', :signtal_type => 'digital'), last
   end
