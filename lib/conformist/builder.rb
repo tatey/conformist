0d4947121063612d3d8b03710976fffd385a4cca
Tate Johnson <tate@tatey.com>
2012-01-05 15:26:21 +1000

    definition -> schema

--- lib/conformist/builder.rb
+++ lib/conformist/builder.rb
@@ -1,7 +1,7 @@
 module Conformist
   class Builder
-    def self.call definition, enumerable
-      columns = definition.columns
+    def self.call schema, enumerable
+      columns = schema.columns
       columns.inject HashStruct.new do |hash, column|
         hash.merge column.name => column.values_in(enumerable)
       end
