20385de8327c73103720e5126ebc06571206144d
Tate Johnson <tate@tatey.com>
2012-01-19 21:21:48 +1000

    Namespace is redundant

--- test/unit/conformist/column_test.rb
+++ test/unit/conformist/column_test.rb
@@ -6,51 +6,51 @@ class ColumnTest < MiniTest::Unit::TestCase
   end
   
   def test_name
-    column = Conformist::Column.new :foo
+    column = Column.new :foo
     assert_equal :foo, column.name
   end
   
   def test_one_index
-    column = Conformist::Column.new :foo, 0
+    column = Column.new :foo, 0
     assert_equal 'a', column.values_in(stub_row)
   end
   
   def test_preprocess_with_one_index
-    column = Conformist::Column.new(:foo, 0) { |value| value.upcase }
+    column = Column.new(:foo, 0) { |value| value.upcase }
     assert_equal 'A', column.values_in(stub_row)
   end
   
   def test_many_indexes
-    column = Conformist::Column.new :foo, 1, 2, 3
+    column = Column.new :foo, 1, 2, 3
     assert_equal ['b', 'c', 'd'], column.values_in(stub_row)
   end
   
   def test_preprocess_with_many_indexes
-    column = Conformist::Column.new(:foo, 1, 2, 3) { |values| values.reverse }
+    column = Column.new(:foo, 1, 2, 3) { |values| values.reverse }
     assert_equal ['d', 'c', 'b'], column.values_in(stub_row)
   end
   
   def test_virtual
-    column = Conformist::Column.new(:foo) { 'a' }
+    column = Column.new(:foo) { 'a' }
     assert_equal 'a', column.values_in(stub_row)
   end
   
   def test_strip_whitespace
-    column = Conformist::Column.new(:foo, 0)
+    column = Column.new :foo, 0
     assert_equal 'a', column.values_in(['  a  '])
   end
   
   def test_array
     mock   = MiniTest::Mock.new
     mock.expect :to_a, ['a']
-    column = Conformist::Column.new :foo, 0
+    column = Column.new :foo, 0
     assert_equal 'a', column.values_in(['a'])
     assert_equal 'a', column.values_in('a')
     assert_equal 'a', column.values_in(mock)
   end
   
   def test_nil
-    column = Conformist::Column.new(:foo, 0)
+    column = Column.new :foo, 0
     assert_nil column.values_in([])
   end
 end
