d363320bb7101112e6061d13aa6409304fdb7bb5
Peter Suschlik <ps@neopoly.de>
2016-10-13 12:35:09 +0200

    Speed up building HashStruct
    Processing large amount of columns is very expensive when building HashStruct
    object over and over again.
    
    This commit removes the need of HashStruct#merge and constructs a HashStruct
    only once.
    
    This also improves performance.

--- test/unit/conformist/hash_struct_test.rb
+++ test/unit/conformist/hash_struct_test.rb
@@ -24,14 +24,6 @@ class Conformist::HashStructTest < Minitest::Test
     refute_equal hash1, hash3
   end
 
-  def test_merge
-    hash1 = HashStruct.new
-    hash2 = hash1.merge :a => 1
-    refute_equal hash1.object_id, hash2.object_id
-    refute_equal 1, hash1[:a]
-    assert_equal 1, hash2[:a]
-  end
-
   def test_readers_with_method_missing
     hash = HashStruct.new :a => 1, :c_d => 1
     assert_equal 1, hash.a
